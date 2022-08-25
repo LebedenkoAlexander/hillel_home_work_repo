a401612171b0fa830dc0d85a5911844f2daf81c5
alexander_lebedenko <masahitokirai@gmail.com>
2022-08-25 15:26:45 +0300

    Lesson 3. Homework. Simple math app

---
+++ js/basic.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const firstRequestResult = prompt("Please enter first digit");
+const secondRequestResult = prompt("Please enter second digit");
+const summOfRequestedDigits = Number(firstRequestResult) + Number(secondRequestResult);
+alert(firstRequestResult + " + " + secondRequestResult + " = " + summOfRequestedDigits);
