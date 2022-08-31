36f8546a03e1f7be3dbe162336fb53d24ccd2c0f
alexander_lebedenko <masahitokirai@gmail.com>
2022-08-31 10:51:51 +0300

    Lesson 3 homework

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,47 @@
 'use strict';
 
-const firstRequestResult = prompt("Please enter first digit");
-const secondRequestResult = prompt("Please enter second digit");
-const summOfRequestedDigits = Number(firstRequestResult) + Number(secondRequestResult);
-alert(firstRequestResult + " + " + secondRequestResult + " = " + summOfRequestedDigits);
+let firstOperandResult = requestOperand("Please enter first operand. If any troubless with the input we will request you again.");
+firstOperandResult = Number(firstOperandResult);
+let secondOperandResult = requestOperand("Please enter second operand. If any troubless with the input we will request you again.");
+secondOperandResult = Number(secondOperandResult);
+const operandsAction = requestActionFromUser("Please enter action you want to do: +, -, /, *. If any troubless with the input we will request you again.");
+
+const operandsActionResult = doMath(firstOperandResult, secondOperandResult, operandsAction);
+const operandsActionResultLine = returnActionResultLine(firstOperandResult, secondOperandResult, operandsAction, operandsActionResult);
+alert(operandsActionResultLine);
+
+
+function requestActionFromUser (title) {
+    let requestResult;
+    do
+    {
+        requestResult = prompt(title);
+    } while (requestResult === null || requestResult === '' || !/^[\/\+\-\*]+$/.test(requestResult))
+    return requestResult;
+}
+
+function requestOperand (title) {
+    let requestResult;
+    do
+    {
+        requestResult = prompt(title);
+    } while (requestResult === null || requestResult === '' || isNaN(+requestResult))
+    return requestResult;
+}
+
+function doMath (firstOperand, secondOperand, mathAction)
+{
+    let actionResult = null;
+    switch(mathAction){
+        case '+': actionResult = firstOperand + secondOperand; break;
+        case '-': actionResult = firstOperand - secondOperand; break;
+        case '/': actionResult = firstOperand / secondOperand; break;
+        case '*': actionResult = firstOperand * secondOperand; break;
+    }
+    return actionResult;
+}
+
+function returnActionResultLine (firstOperand, secondOperand, mathAction, mathResult)
+{
+    return(`${firstOperand} ${mathAction} ${secondOperand} = ${mathResult}`);
+}
