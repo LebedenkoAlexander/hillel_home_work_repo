eed5820983a0602fdfb37d6ed44d5fefa2907d34
alexander_lebedenko <masahitokirai@gmail.com>
2022-09-05 17:24:44 +0300

    lesson_6_hw

--- js/basic.js
+++ js/basic.js
@@ -1,47 +1,91 @@
-'use strict';
+"use strict";
 
-let firstOperandResult = requestOperand("Please enter first operand. If any troubless with the input we will request you again.");
-firstOperandResult = Number(firstOperandResult);
-let secondOperandResult = requestOperand("Please enter second operand. If any troubless with the input we will request you again.");
-secondOperandResult = Number(secondOperandResult);
-const operandsAction = requestActionFromUser("Please enter action you want to do: +, -, /, *. If any troubless with the input we will request you again.");
-
-const operandsActionResult = doMath(firstOperandResult, secondOperandResult, operandsAction);
-const operandsActionResultLine = returnActionResultLine(firstOperandResult, secondOperandResult, operandsAction, operandsActionResult);
+//#region main
+const operandsAction = requestActionFromUser(
+  "Please enter action you want to do: +, -, /, *.\r\nIf any troubless with the input we will request you again."
+);
+const actionsCount = requestActionsCount(
+  "Please specify how much times you want do the requested action.\r\nImportant! 1 or more is expected.\r\nIf any troubless with the input we will request you again."
+);
+const operandsArray = requestOperands(actionsCount);
+const resultOfActionsForOperands = doMath(operandsAction, operandsArray);
+const operandsActionResultLine = returnActionResultLine(
+  operandsAction,
+  operandsArray,
+  resultOfActionsForOperands
+);
 alert(operandsActionResultLine);
+//#endregion
 
-
-function requestActionFromUser (title) {
-    let requestResult;
-    do
-    {
-        requestResult = prompt(title);
-    } while (requestResult === null || requestResult === '' || !/^[\/\+\-\*]+$/.test(requestResult))
-    return requestResult;
+//#region Functions
+function requestActionFromUser(title) {
+  let requestResult;
+  do {
+    requestResult = prompt(title);
+  } while (
+    requestResult === null ||
+    requestResult === "" ||
+    !/^[\/\+\-\*]+$/.test(requestResult)
+  );
+  return requestResult;
 }
 
-function requestOperand (title) {
-    let requestResult;
-    do
-    {
-        requestResult = prompt(title);
-    } while (requestResult === null || requestResult === '' || isNaN(+requestResult))
-    return requestResult;
+function requestActionsCount(title) {
+  let requestResult;
+  do {
+    requestResult = prompt(title);
+  } while (
+    requestResult === null ||
+    requestResult === "" ||
+    isNaN(+requestResult) ||
+    +requestResult <= 0
+  );
+  return +requestResult;
 }
 
-function doMath (firstOperand, secondOperand, mathAction)
-{
-    let actionResult = null;
-    switch(mathAction){
-        case '+': actionResult = firstOperand + secondOperand; break;
-        case '-': actionResult = firstOperand - secondOperand; break;
-        case '/': actionResult = firstOperand / secondOperand; break;
-        case '*': actionResult = firstOperand * secondOperand; break;
+function requestOperands(operandsAmount) {
+  let operandsArray = [];
+  let requestResult;
+  for (let i = 0; i <= operandsAmount; i++) {
+    do {
+      requestResult = prompt(`Please enter operand № ${i + 1}`);
+    } while (
+      requestResult === null ||
+      requestResult === "" ||
+      isNaN(+requestResult)
+    );
+    operandsArray.push(+requestResult);
+  }
+  return operandsArray;
+}
+
+function doMath(mathAction, operandsArray) {
+  let actionResult = operandsArray[0];
+  for (let i = 1; i < operandsArray.length; i++) {
+    switch (mathAction) {
+      case "+":
+        actionResult += operandsArray[i];
+        break;
+      case "-":
+        actionResult -= operandsArray[i];
+        break;
+      case "/":
+        actionResult /= operandsArray[i];
+        break;
+      case "*":
+        actionResult *= operandsArray[i];
+        break;
     }
-    return actionResult;
+  }
+  return actionResult;
 }
 
-function returnActionResultLine (firstOperand, secondOperand, mathAction, mathResult)
-{
-    return(`${firstOperand} ${mathAction} ${secondOperand} = ${mathResult}`);
+function returnActionResultLine(action, operandsArray, actionsResult) {
+  let resultedLine = `(${operandsArray[0]})`;
+  for (let i = 1; i < operandsArray.length; i++) {
+    resultedLine += ` ${action} (${operandsArray[i]})`;
+  }
+  resultedLine += ` = ${actionsResult}`;
+  return resultedLine;
 }
+//#endregion
