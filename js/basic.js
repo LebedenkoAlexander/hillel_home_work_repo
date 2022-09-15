658e6c4576a6f0b6da863d3fe38606b603eb777c
alexander_lebedenko <masahitokirai@gmail.com>
2022-09-15 14:43:45 +0300

    clear project

--- js/basic.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const firstRequestResult = prompt("Please enter first digit");
-const secondRequestResult = prompt("Please enter second digit");
-const summOfRequestedDigits = Number(firstRequestResult) + Number(secondRequestResult);
-alert(firstRequestResult + " + " + secondRequestResult + " = " + summOfRequestedDigits);
