570fc2d868b9221a07b199741befbe40cafa6a83
LebedenkoAlexander <masahitokirai@gmail.com>
2022-10-04 16:35:04 +0300

    calc using class function

--- js/script.js
+++ js/script.js
@@ -4,23 +4,28 @@
 //#endregion
 
 //#region main
-const calc = createCalculator(10);
-console.log(calc.sum(5)); /// 15
-console.log(calc.mult(10)); // 150
-console.log(calc.sub(40)); // 110
-console.log(calc.div(10)); // 11
-console.log(calc.set(100)); //
+const calc = new Calc(10);
+calc.sum(5); /// 15
+calc.mult(10); // 150
+calc.sub(40); // 110
+calc.div(10); // 11
+console.log(calc.resultedValue);
 //#endregion
 
 //#region functions
-function createCalculator(startValue) {
-  let resultedValue = startValue;
-  return {
-    sum: (valueToAdd) => (resultedValue += valueToAdd),
-    mult: (valueToMultiply) => (resultedValue *= valueToMultiply),
-    sub: (valueToSubstract) => (resultedValue -= valueToSubstract),
-    div: (valueToDivide) => (resultedValue /= valueToDivide),
-    set: (valueToSet) => (resultedValue = valueToSet),
+function Calc(initValue) {
+  this.resultedValue = initValue;
+  this.sum = function (valueToAdd) {
+    this.resultedValue += valueToAdd;
+  };
+  this.mult = function (valueToMultiply) {
+    this.resultedValue *= valueToMultiply;
+  };
+  this.sub = function (valueToSubstract) {
+    this.resultedValue -= valueToSubstract;
+  };
+  this.div = function (valueToDivide) {
+    this.resultedValue /= valueToDivide;
   };
 }
 //#endregion
